47bc0e146144181661f1ed9ac7fe1b7264d27a0e
li <285085538@qq.com>
2019-11-28 18:53:40 +0800

    no2

--- src/main/java/com/it/git/Happy.java
+++ src/main/java/com/it/git/Happy.java
@@ -3,6 +3,6 @@ package com.it.git;
 public class Happy {
 public static void main(String[] args) {
 	System.out.println("happy...");
-	System.out.println("lllls");
+	System.out.println("llllsd");
 }
 }
